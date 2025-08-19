2b238132abc3a38d089e24a9cc2eb3adda4ff710
santirms <santiago.r89@gmail.com>
2025-08-19 00:37:49 -0300

    Update asignaciones.js

--- routes/asignaciones.js
+++ routes/asignaciones.js
@@ -14,8 +14,11 @@ router.post('/mapa', asignarViaMapa);
 //HISTORIAL REMITOS
 router.get('/', listarAsignaciones);
 router.get('/:id', detalleAsignacion);
+router.get('/:id/whatsapp', whatsappLink);
 router.patch('/:id/remove', quitarEnvios);
 router.patch('/:id/move', moverEnvios);
 router.patch('/:id/add', agregarEnvios);      // agregar a un remito
+
+router.delete('/:id', eliminarAsignacion);
 
 module.exports = router;
