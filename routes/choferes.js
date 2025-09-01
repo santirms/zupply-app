1dbb91445103e325d88ec951db3a4ba029bfe20e
santirms <santiago.r89@gmail.com>
2025-08-31 21:07:07 -0300

    Update choferes.js

--- routes/choferes.js
+++ routes/choferes.js
@@ -1,4 +1,3 @@
-// routes/choferes.js
 const router = require('express').Router();
 const { requireAuth, requireRole } = require('../middlewares/auth');
 const ctrl   = require('../controllers/choferController');
@@ -7,6 +6,8 @@ router.use(requireAuth);
 router.get('/asignados', requireRole('admin','coordinador'), ctrl.asignadosDelDia);
 router.post('/asignar-por-qr', requireRole('admin','coordinador'), ctrl.asignarPorQR);
 router.post('/asignar-desde-mapa', requireRole('admin','coordinador'), ctrl.asignarDesdeMapa);
+
+// CRUD choferes
 router.get('/',  ctrl.listarChoferes);
 router.post('/', ctrl.crearChofer);
 
