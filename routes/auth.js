4ed4e2318814af039141ed0df18fe9e95441d64e
santirms <santiago.r89@gmail.com>
2025-09-12 01:02:47 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/authController');
-
-console.log('authController is', typeof ctrl, ctrl && Object.keys(ctrl));
-
 router.post('/login', ctrl.login);
 router.post('/logout', ctrl.logout);
 router.get('/me',      ctrl.me);
