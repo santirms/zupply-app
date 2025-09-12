abdf724d0b6fae65b9e756c466da97b86b6c89f9
santirms <santiago.r89@gmail.com>
2025-09-12 00:33:01 -0300

    Update envios.js

--- routes/envios.js
+++ routes/envios.js
@@ -15,7 +15,12 @@ const { getEnvioByTracking, labelByTracking } = require('../controllers/envioCon
 const ctrl   = require('../controllers/envioController');
 
 // ⬇️ NUEVO: middlewares
-const { requireAuth, restrictMethodsForRoles, requireRole } = require('../middlewares/auth');
+const { + const {
++   requireAuth,
++   requireRole,
++   onlyOwnShipments,
++   onlyManualOrEtiqueta
++ } = require('../middlewares/auth');
 
 // ⬇️ TODO EL PANEL GENERAL REQUIERE LOGIN
 router.use(requireAuth);
