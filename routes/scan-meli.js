d4f04edcadb93bbbd6ffca509cda0c34497288ac
santirms <santiago.r89@gmail.com>
2025-10-28 02:28:04 -0300

    Update scan-meli.js

--- routes/scan-meli.js
+++ routes/scan-meli.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/scanMeliController');
 
-router.post('/', ctrl.scanAndUpsert);                 // /api/scan-meli
+router.post('/', ctrl.scanMeli);                 // /api/scan-meli
 router.get('/latest-render/:id', ctrl.latestQr);      // /api/scan-meli/latest-render/:id
 
 module.exports = router;
