03ff2c07b85ab600038877f357717299fa9bdb68
santirms <santiago.r89@gmail.com>
2025-11-03 00:37:13 -0300

    Revert "Remove all "en_preparacion" status references"

--- models/ZonaPorCP.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const zonaPorCPSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  codigos_postales: { type: [String], required: true }
-});
-
-module.exports = mongoose.model('ZonaPorCP', zonaPorCPSchema);
--- routes/scan-manual.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/scanMeliController');
-const { optionalAuth } = require('../middlewares/auth');
-
-router.post('/', ctrl.scanMeli);
-
-module.exports = router;
