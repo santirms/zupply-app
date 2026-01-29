f4265d01f9615c4c73867d00d3f8d4e3ed1be322
santirms <santiago.r89@gmail.com>
2026-01-28 21:33:34 -0300

    Update scan-manual.js

--- routes/scan-manual.js
+++ routes/scan-manual.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/scanMeliController');
 const { optionalAuth } = require('../middlewares/auth');
+const identifyTenant = require('../middlewares/identifyTenant');
 
-router.post('/', ctrl.scanMeli);
+router.post('/', identifyTenant, ctrl.scanMeli);
 
 module.exports = router;
