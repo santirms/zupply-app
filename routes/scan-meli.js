c1b3dde2d020c468ea6d30955653c06aa4424a61
santirms <santiago.r89@gmail.com>
2026-01-28 21:32:56 -0300

    Update scan-meli.js

--- routes/scan-meli.js
+++ routes/scan-meli.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/scanMeliController');
+const identifyTenant = require('../middlewares/identifyTenant');
 
-router.post('/', ctrl.scanAndUpsert);                 // /api/scan-meli << cambiar esta linea por "scanMeli" para el flujo de qrs o id internos
+router.post('/', identifyTenant, ctrl.scanAndUpsert);                 // /api/scan-meli << cambiar esta linea por "scanMeli" para el flujo de qrs o id internos
 router.get('/latest-render/:id', ctrl.latestQr);      // /api/scan-meli/latest-render/:id
 
 module.exports = router;
